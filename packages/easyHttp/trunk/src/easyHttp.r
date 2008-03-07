8070a90229bc9625056bac90535efe4fb305c6f0
andyfaff <andyfaff@17ec3ff0-b92a-0410-b65e-9095fddf55e2>
2008-03-07 20:33:16 +0000

    changed getHttp to easyHttp

---
+++ packages/easyHttp/trunk/src/easyHttp.r
@@ -0,0 +1,52 @@
+#include "XOPStandardHeaders.r"
+
+resource 'vers' (1) {						/* XOP version info */
+	0x01, 0x00, final, 0x00, 0,				/* version bytes and country integer */
+	"1.00",
+	"1.00, © 1993 WaveMetrics, Inc., all rights reserved."
+};
+
+resource 'vers' (2) {						/* Igor version info */
+	0x05, 0x04, release, 0x00, 0,			/* version bytes and country integer */
+	"5.04",
+	"(for Igor Pro 2.00 or later)"
+};
+
+resource 'STR#' (1100) {					/* custom error messages */
+	{
+		/* [1] */
+		"easyhttp requires Igor Pro 5.0 or later.",
+
+	}
+};
+
+/* no menu item */
+
+resource 'XOPI' (1100) {
+	XOP_VERSION,							// XOP protocol version.
+	DEV_SYS_CODE,							// Development system information.
+	0,										// Obsolete - set to zero.
+	0,										// Obsolete - set to zero.
+	XOP_TOOLKIT_VERSION,					// XOP Toolkit version.
+};
+
+//resource 'XOPF' (1100) {
+//	{
+		/* str1 = iPeekGetPeekData(host, port, instrument) */	/* This uses the direct call method */
+//		"iPeekGetPeekData",							/* function name */
+//		F_IO | F_EXTERNAL,					/* function category (string) */
+//		HSTRING_TYPE,						/* return value type str1 (string handle) */			
+//		{
+		//	HSTRING_TYPE,					/* host (string handle) */
+		//	NT_FP64,                         /* port (string handle) */
+        //    HSTRING_TYPE,                   /* instrument (string handle) */
+//		},
+//	}
+//};
+
+resource 'XOPC' (1100) {
+	{
+		"easyhttp",								// Name of operation.
+		XOPOp+UtilOP+compilableOp,			// Operation's category.
+	}
+};
