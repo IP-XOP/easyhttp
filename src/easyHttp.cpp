93c0898449f59744bbe0da9885548d9a9ac7012d
andyfaff <andyfaff@17ec3ff0-b92a-0410-b65e-9095fddf55e2>
2008-10-22 04:14:44 +0000

    checking in new version

--- src/easyHttp.cpp
+++ src/easyHttp.cpp
@@ -131,6 +131,7 @@ ExecuteEasyHTTP(easyHttpRuntimeParamsPtr p)
 		
 		curl_easy_setopt(curl, CURLOPT_UPLOAD, 1) ;
 		curl_easy_setopt(curl, CURLOPT_READDATA, inputFile);
+		curl_easy_setopt(curl, CURLOPT_FTP_CREATE_MISSING_DIRS, 1L);
 	}
 	
 	/* Do you want to save the file to disc? */
